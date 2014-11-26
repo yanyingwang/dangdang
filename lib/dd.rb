8d0ef8301ee88651c5df1071a9a8241592c0e407
YanyingWang <yanyingwangwang@gmail.com>
2014-11-26 17:01:22 +0800

    order list and order detail

--- lib/dd.rb
+++ lib/dd.rb
@@ -1,7 +1,8 @@
-require 'faraday'
 require 'digest'
 require 'pry'
 require 'cgi'
+require 'eat'
+require 'active_support/core_ext/hash'
 
 class Dangdang
   def initialize(option = {})
@@ -9,35 +10,47 @@ class Dangdang
     @api_uri_front = "http://api.open.dangdang.com/openapi/rest?"
   end
   def sys_params
-    { "app_key" => @option["app_key"],
-      "format" => "xml",
-      "method" => @option["method"],
-      "session" => @option["session_key"],
-      "sign_method" => "md5",
-      "timestamp" => Time.now.strftime("%Y-%m-%d %T"),
-      "v" => "1.0" }
+    { :app_key => @option[:app_key],
+      :format => "xml",
+      :method => @method,
+      :session => @option[:session_key],
+      :sign_method => "md5",
+      :timestamp => Time.now.strftime("%Y-%m-%d %T"),
+      :v => "1.0" }
   end
 
   def sys_params_to_string
     sys_params.to_a.flatten.join("")
   end
   def secret_string
-    @option["app_secret"] + sys_params_to_string + @option["app_secret"]
+    @option[:app_secret] + sys_params_to_string + @option[:app_secret]
   end
   def sign_key
     Digest::MD5.hexdigest(secret_string).upcase
   end
 
-  def url_params
-    params = sys_params
-    params["timestamp"] = CGI.escape(params["timestamp"])
-    params.store('osd', CGI.escape("2014-11-25 00:00:00"))
-    params.store('oed', CGI.escape("2014-11-26 00:00:00"))
-    params.to_a.map { |p| p.join("=")}.join("&")
+
+  def dangdang_orders_list_get(params_app = {})
+    @method = __callee__.to_s.split("_").join(".")
+    params_app[:osd] = CGI.escape(params_app[:osd]) if params_app[:osd]
+    params_app[:oed] = CGI.escape(params_app[:oed]) if params_app[:oed]
+    @params_app = params_app
+    Hash.from_xml(eat(get_api_uri))
+  end
+  def dangdang_order_details_get(params_app = {})
+    @method = __callee__.to_s.split("_").join(".")
+    @params_app = params_app
+    Hash.from_xml(eat(get_api_uri))
   end
 
   def get_api_uri
     @api_uri_front + "sign=#{sign_key}&" + url_params
+  end
+  def url_params
+    params = sys_params
+    params.merge!(@params_app)
+    params[:timestamp] = CGI.escape(params[:timestamp])
+    params.to_a.map { |p| p.join("=")}.join("&")
   end
 
 end
